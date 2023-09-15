81054658aedbcbb3a410aa8849c39bddb732c88c
fraqs <fmaster5005@gmail.com>
2023-09-15 13:27:58 -0300

    version 2

--- app/src/main/java/com/gmail/fmaster5005/autocar/MainActivity.kt
+++ app/src/main/java/com/gmail/fmaster5005/autocar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gmail.fmaster5005.autocar
 import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import android.widget.Button
 import android.view.MotionEvent
@@ -12,73 +13,41 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.marginLeft
 import androidx.core.view.marginRight
 import androidx.core.view.marginTop
+import kotlinx.coroutines.delay
+import kotlin.concurrent.timer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tmp: Float = 0f
-        var tmp1: Float = 0f
-
-        val textField = findViewById<TextView>(R.id.textView3)
-        val button: Button = findViewById(R.id.buttonControllers)
-        button.setOnTouchListener { view, event ->
-
-            when(event.action){
-                MotionEvent.ACTION_DOWN -> {
-                    tmp = event.rawX - view.x
-                    tmp1 = event.rawY - 72f - view.y
-                }
-
-                MotionEvent.ACTION_MOVE -> {
-                    view.x = event.rawX - tmp
-                    view.y = event.rawY  - 72f - tmp1
-                }
-            }
-
-
-            textField.text = "(${tmp}, ${tmp1})"
-
-
-
-            true
+        val resourceId2 = resources.getIdentifier("status_bar_height", "dimen", "android")
+        var statusBarHeight = 0
+        if (resourceId2 > 0) {
+            statusBarHeight = resources.getDimensionPixelSize(resourceId2)
         }
 
-        val joystick_fc: Button = findViewById(R.id.joystick_fc)
+        val resourceId1: Int = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        var navigationBarHeight = 0
+        if (resourceId1 > 0) {
+            navigationBarHeight = resources.getDimensionPixelSize(resourceId1)
+        }
 
-        val joystick: Button = findViewById(R.id.joystick)
-        joystick.setOnTouchListener { view, event ->
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        val dpi = metrics.densityDpi
 
-            val joystickCoordX = (event.rawX  - joystick.width/2f - view.x).toDouble()
-            val joystickCoordY = (event.rawY - 72f - joystick.width/2f - view.y).toDouble()
-            val joystickRadius = (joystick.width/2f).toDouble()
+        val btn: Button = findViewById(R.id.button)
+        val txt: TextView = findViewById(R.id.textView)
 
-            if(Math.pow(joystickCoordX, 2.0) + Math.pow(joystickCoordY, 2.0) <= Math.pow(joystickRadius, 2.0)){
-                when(event.action) {
-                    MotionEvent.ACTION_MOVE -> {
-                        joystick_fc.y = event.rawY - 72f - joystick_fc.width/2f
-                        joystick_fc.x = event.rawX - joystick_fc.width/2f
-                    }
+        btn.setOnTouchListener { view, event ->
 
-                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                        joystick_fc.y = view.y + joystick.width/2f - joystick_fc.width/2f
-                        joystick_fc.x = view.x + joystick.width/2f - joystick_fc.width/2f
-                    }
-                }
-            }
-            else{
-                joystick_fc.y = view.y + joystick.width/2f - joystick_fc.width/2f
-                joystick_fc.x = view.x + joystick.width/2f - joystick_fc.width/2f
-            }
+            view.x = event.rawX
+            view.y = event.rawY - statusBarHeight
+
+            txt.text = "(${event.rawX}, ${event.rawY})"
 
             true
-        }
-        joystick.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {
-                joystick_fc.y
-                1 +1
-            }
         }
     }
 }
